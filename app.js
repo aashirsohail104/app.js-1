0d9e703ad435bd0f0ec4294d87d569173a797aad
AASHIR SIDDIQUI <154249806+aashirsohail104@users.noreply.github.com>
2024-01-24 03:00:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+ alert("Hello to the Page")
+ confirm("Do you want to proceed?")
+ var inp = prompt("Enter the Number")
+ 
+ console.log(inp)
+ document.write(`${inp} x 1 = `, inp*1 + "<br>")
+ document.write(`${inp} x 2 = `, inp*2 + "<br>")
+ document.write(`${inp} x 3 = `, inp*3 + "<br>")
+ document.write(`${inp} x 4 = `, inp*4 + "<br>")
+ document.write(`${inp} x 5 = `, inp*5 + "<br>")
+ document.write(`${inp} x 6 = `, inp*6 + "<br>")
+ document.write(`${inp} x 7 = `, inp*7 + "<br>")
+ document.write(`${inp} x 8 = `, inp*8 + "<br>")
+ document.write(`${inp} x 9 = `, inp*9 + "<br>")
+ document.write(`${inp} x 10 = `, inp*10 + "<br>")
+
+ console.log
+
+  console.log(`${inp} x 1 = `, inp*1 + "<br>")
+  console.log(`${inp} x 2 = `, inp*2 + "<br>")
+  console.log(`${inp} x 3 = `, inp*3 + "<br>")
+  console.log(`${inp} x 4 = `, inp*4 + "<br>")
+  console.log(`${inp} x 5 = `, inp*5 + "<br>")
+  console.log(`${inp} x 6 = `, inp*6 + "<br>")
+  console.log(`${inp} x 7 = `, inp*7 + "<br>")
+  console.log(`${inp} x 8 = `, inp*8 + "<br>")
+  console.log(`${inp} x 9 = `, inp*9 + "<br>")
+  console.log(`${inp} x 10 = `, inp*10 + "<br>")
+
+
